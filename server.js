fb4746e9b11e30af1ec6c06f367b266525773f71
makcimerrr <maximedubs@proton.me>
2025-02-01 17:58:03 +0100

    chore: edit path api-docs

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use(cors({
     }
 }));
 
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
     customCss: `
         .swagger-ui .topbar {
             display: none;
