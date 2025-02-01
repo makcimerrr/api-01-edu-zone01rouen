22ef3e6ff94ab5d392abdbb69c025d7eed3151d8
makcimerrr <maximedubs@proton.me>
2025-02-01 17:40:44 +0100

    chore: init docs

---
+++ swagger.js
@@ -0,0 +1,25 @@
+// swagger.js
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'API de Maxime Dubois',
+        version: '1.0.0',
+        description: 'Une API pour récupérer des informations sur les utilisateurs et la progression des promotions.',
+    },
+    servers: [
+        {
+            url: 'http://localhost:3010',
+        },
+    ],
+};
+
+const options = {
+    swaggerDefinition,
+    apis: ['./server.js'], // fichier où se trouvent les commentaires des routes
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;
