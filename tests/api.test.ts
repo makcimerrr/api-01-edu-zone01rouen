91d9e13bdba3ce79d6fc42c7ada955ef32684f19
makcimerrr <maximedubs@proton.me>
2025-02-04 00:45:20 +0100

    fix: update test for GET /users to check for non-empty response

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,16 +1,17 @@
 // api.test.ts
-import {assertEquals} from "https://deno.land/std/testing/asserts.ts";
-import {createApp} from "https://deno.land/x/oak/mod.ts";
-import router from "../src/route/routes.ts";
+import {assert, assertEquals} from "https://deno.land/std@0.224.0/assert/mod.ts";
+import {Application} from "https://deno.land/x/oak/mod.ts";
+import router from "../src/route/route.ts";
 
 Deno.test("GET /api/v1/users should return a list of users", async () => {
-    const app = createApp();
+    const app = new Application();
     app.use(router.routes());
     app.use(router.allowedMethods());
 
     const response = await fetch("http://localhost:8000/api/v1/users");
-    const users = await response.json();
+    const responseBody = await response.json();
 
     assertEquals(response.status, 200);
-    assertEquals(users.length, 2);
+    assert(Array.isArray(responseBody.data.user));  // Vérifie que c'est bien un tableau
+    assert(responseBody.data.user.length > 0);  // Vérifie que le tableau n'est pas vide
 });
