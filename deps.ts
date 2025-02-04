ddd850e3b9bfbb5ccf626aab27bd0c5ab479e567
makcimerrr <maximedubs@proton.me>
2025-02-04 09:24:31 +0100

    chore: install function from dependencie

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 // deps.ts
-export {Application, Router} from "https://deno.land/x/oak/mod.ts";
+export {Application, Router, send} from "https://deno.land/x/oak/mod.ts";
 export {CONFIG} from "./config/config.ts";
 export {storage} from "./config/storage.ts";
