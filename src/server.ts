fb903dc448de31ed6e6a3d51fb2aae9b16154e36
makcimerrr <maximedubs@proton.me>
2025-02-04 09:24:04 +0100

    feat(server): create home page app

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 // server.ts
-import {Application} from "../deps.ts";
+import {Application, send} from "../deps.ts";
 import router from "./route/route.ts";
 
 const app = new Application();
+
+// Route pour servir le fichier index.html et style.css
+app.use(async (ctx: { request: { url: { pathname: string; }; }; }, next: () => any) => {
+    if (ctx.request.url.pathname === "/") {
+        await send(ctx, "index.html", {root: "./src/public"});
+    } else if (ctx.request.url.pathname === "/style.css") {
+        await send(ctx, "style.css", {root: "./src/public"});
+    } else {
+        await next();
+    }
+});
 
 // Routes
 app.use(router.routes());
