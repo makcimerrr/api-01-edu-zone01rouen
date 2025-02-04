bcb5500194121b2e914aa441bbb942bc9e68803a
makcimerrr <maximedubs@proton.me>
2025-02-04 02:23:30 +0100

    fix: UNCAUGHT_EXCEPTION TypeError: module not found

--- src/route/route.ts
+++ src/route/route.ts
@@ -8,9 +8,9 @@ const router = new Router();
 // Fonction asynchrone pour gérer l'importation dynamique des contrôleurs
 async function loadUserController() {
     // Dynamique en fonction de la version
-    const usersController = await import(`./${API_BASE_PATH}/${API_VERSION}/user.ts`);
-    const promotionsController = await import(`./${API_BASE_PATH}/${API_VERSION}/promotion.ts`);
-    const giteaController = await import(`./${API_BASE_PATH}/${API_VERSION}/gitea.ts`);
+    const usersController = await import(`../${API_BASE_PATH}/${API_VERSION}/user.ts`);
+    const promotionsController = await import(`../${API_BASE_PATH}/${API_VERSION}/promotion.ts`);
+    const giteaController = await import(`../${API_BASE_PATH}/${API_VERSION}/gitea.ts`);
 
     // Routes pour les utilisateurs`
     router.get(`/${API_BASE_PATH}/${API_VERSION}/users`, usersController.getUsers)
