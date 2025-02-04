2d58d537ebe3703afe1addf57ca6bf7c532dea50
makcimerrr <maximedubs@proton.me>
2025-02-04 02:51:37 +0100

    refactor: undo commit

--- src/route/route.ts
+++ src/route/route.ts
@@ -1,6 +1,6 @@
 // routes.ts
 import {Router} from "../../deps.ts";
-import {BASE_PATH, VERSION_ACTUAL} from "../../config/config.ts"; // Import des constantes
+import {API_BASE_PATH, API_VERSION} from "../../config/config.ts"; // Import des constantes
 import {checkToken} from "../utils/token.ts"
 
 const router = new Router();
@@ -8,12 +8,10 @@ const router = new Router();
 // Fonction asynchrone pour gérer l'importation dynamique des contrôleurs
 async function loadUserController() {
     try {
-        const base = BASE_PATH;
-        const version = VERSION_ACTUAL;
         // Dynamique en fonction de la version
-        const usersController = await import(`../${base}/${version}/user.ts`);
-        const promotionsController = await import(`../${base}/${version}/promotion.ts`);
-        const giteaController = await import(`../${base}/${version}/gitea.ts`);
+        const usersController = await import(`../api/v1/user.ts`);
+        const promotionsController = await import(`../api/v1/promotion.ts`);
+        const giteaController = await import(`../api/v1/gitea.ts`);
 
         // Routes pour les utilisateurs`
         router.get(`/${API_BASE_PATH}/${API_VERSION}/users`, usersController.getUsers)
