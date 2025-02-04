7903759bb729cb29c0bbf5688815230c0f9b5028
makcimerrr <maximedubs@proton.me>
2025-02-04 02:25:00 +0100

    fix: debugging

--- src/route/route.ts
+++ src/route/route.ts
@@ -7,21 +7,24 @@ const router = new Router();
 
 // Fonction asynchrone pour gérer l'importation dynamique des contrôleurs
 async function loadUserController() {
-    // Dynamique en fonction de la version
-    const usersController = await import(`../${API_BASE_PATH}/${API_VERSION}/user.ts`);
-    const promotionsController = await import(`../${API_BASE_PATH}/${API_VERSION}/promotion.ts`);
-    const giteaController = await import(`../${API_BASE_PATH}/${API_VERSION}/gitea.ts`);
+    try {
+        // Dynamique en fonction de la version
+        const usersController = await import(`../${API_BASE_PATH}/${API_VERSION}/user.ts`);
+        const promotionsController = await import(`../${API_BASE_PATH}/${API_VERSION}/promotion.ts`);
+        const giteaController = await import(`../${API_BASE_PATH}/${API_VERSION}/gitea.ts`);
 
-    // Routes pour les utilisateurs`
-    router.get(`/${API_BASE_PATH}/${API_VERSION}/users`, usersController.getUsers)
-        .get(`/${API_BASE_PATH}/${API_VERSION}/user-info/:username`, usersController.getUserInfo)
-        .get(`/${API_BASE_PATH}/${API_VERSION}/promotions/:eventId/students`, promotionsController.getPromotionProgress)
-        .get(`/${API_BASE_PATH}/${API_VERSION}/gitea-info/:username`, checkToken, giteaController.getUserInfoFromGitea)
-    /*.get(`/${API_BASE_PATH}/${API_VERSION}/users/:id`, usersController.getUserById)
-    .post(`/${API_BASE_PATH}/${API_VERSION}/users`, usersController.createUser)
-    .put(`/${API_BASE_PATH}/${API_VERSION}/users/:id`, usersController.updateUser)
-    .delete(`/${API_BASE_PATH}/${API_VERSION}/users/:id`, usersController.deleteUser)*/
-    ;
+        // Routes pour les utilisateurs`
+        router.get(`/${API_BASE_PATH}/${API_VERSION}/users`, usersController.getUsers)
+            .get(`/${API_BASE_PATH}/${API_VERSION}/user-info/:username`, usersController.getUserInfo)
+            .get(`/${API_BASE_PATH}/${API_VERSION}/promotions/:eventId/students`, promotionsController.getPromotionProgress)
+            .get(`/${API_BASE_PATH}/${API_VERSION}/gitea-info/:username`, checkToken, giteaController.getUserInfoFromGitea);
+        /*.get(`/${API_BASE_PATH}/${API_VERSION}/users/:id`, usersController.getUserById)
+        .post(`/${API_BASE_PATH}/${API_VERSION}/users`, usersController.createUser)
+        .put(`/${API_BASE_PATH}/${API_VERSION}/users/:id`, usersController.updateUser)
+        .delete(`/${API_BASE_PATH}/${API_VERSION}/users/:id`, usersController.deleteUser)*/
+    } catch (error) {
+        console.error('Error loading controllers:', error);
+    }
 }
 
 loadUserController().then(r => r); // Appel de la fonction pour charger le contrôleur
