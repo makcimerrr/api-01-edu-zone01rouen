1e337caeeaa61e241a0ab170fc976e85d5418d95
makcimerrr <maximedubs@proton.me>
2025-02-04 01:29:30 +0100

    feat: add route for fetching user info and heatmap from Gitea with token validation

--- src/route/route.ts
+++ src/route/route.ts
@@ -1,6 +1,7 @@
 // routes.ts
 import {Router} from "../../deps.ts";
 import {API_BASE_PATH, API_VERSION} from "../../config/config.ts"; // Import des constantes
+import {checkToken} from "../utils/token.ts"
 
 const router = new Router();
 
@@ -9,11 +10,13 @@ async function loadUserController() {
     // Dynamique en fonction de la version
     const usersController = await import(`../${API_BASE_PATH}/${API_VERSION}/user.ts`);
     const promotionsController = await import(`../${API_BASE_PATH}/${API_VERSION}/promotion.ts`);
+    const giteaController = await import(`../${API_BASE_PATH}/${API_VERSION}/gitea.ts`);
 
     // Routes pour les utilisateurs`
     router.get(`/${API_BASE_PATH}/${API_VERSION}/users`, usersController.getUsers)
         .get(`/${API_BASE_PATH}/${API_VERSION}/user-info/:username`, usersController.getUserInfo)
         .get(`/${API_BASE_PATH}/${API_VERSION}/promotion-progress/:eventId`, promotionsController.getPromotionProgress)
+        .get(`/${API_BASE_PATH}/${API_VERSION}/gitea-info/:username`, checkToken, giteaController.getUserInfoFromGitea)
     /*.get(`/${API_BASE_PATH}/${API_VERSION}/users/:id`, usersController.getUserById)
     .post(`/${API_BASE_PATH}/${API_VERSION}/users`, usersController.createUser)
     .put(`/${API_BASE_PATH}/${API_VERSION}/users/:id`, usersController.updateUser)
